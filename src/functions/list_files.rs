fc05a1f8d1528e478b5fe534a909e427d4e7aacc
Lorne Day <77252924+4Lorne@users.noreply.github.com>
2023-12-03 22:16:27 -0400

    fixed loop issue

--- src/functions/list_files.rs
+++ src/functions/list_files.rs
@@ -23,37 +23,41 @@ pub fn list_files(directory: String) {
         .collect();
 
     if file_paths.is_empty() {
-        println!("No files found in {}", directory);
-        std::process::exit(0);
+        println!("No files were found in the directory: {}", directory);
+        new_file();
+        return;
+    } else {
+        file_paths.push(String::from("Create a new file"));
+
+        let selection = Select::new()
+            .with_prompt("Select an existing file:")
+            .default(0)
+            .items(&file_paths)
+            .interact()
+            .unwrap();
+
+        if file_paths[selection] == "Create a new file" {
+            new_file()
+        } else {
+            open_file(&file_paths[selection]).expect("TODO: panic message");
+        }
     }
-    file_paths.push(String::from("Create a new file"));
+}
 
-    let selection = Select::new()
-        .with_prompt("Select an existing file:")
-        .default(0)
-        .items(&file_paths)
-        .interact()
-        .unwrap();
+fn new_file() {
+    println!("Name your file:");
+    let mut s = String::new();
 
-    if file_paths[selection] == "Create a new file" {
+    let _ = stdout().flush();
+    stdin().read_line(&mut s).expect("Error");
+
+    while s.trim().is_empty() {
+        println!("File name cannot be empty");
         println!("Name your file:");
-        let mut s = String::new();
 
         let _ = stdout().flush();
         stdin().read_line(&mut s).expect("Error");
-
-        while s.trim().is_empty() {
-            println!("File name cannot be empty");
-            println!("Name your file:");
-
-            let _ = stdout().flush();
-            stdin().read_line(&mut s).expect("Error");
-        }
-
-        //TODO check if file already exists
-
-        create_file(&s.trim().to_string());
-    } else {
-        open_file(&file_paths[selection]).expect("TODO: panic message");
     }
+
+    create_file(&s.trim().to_string());
 }
